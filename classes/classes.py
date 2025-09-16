f8b9085cfaf92e3efd1338d31d445bd5c0a9178c
Jur van Toledo <jurvantoledo@live.nl>
2025-09-16 10:01:09 +0200

    Fixed loading and transfering in STL to SCL

--- classes/classes.py
+++ classes/classes.py
@@ -29,9 +29,11 @@ Expr = Union[Var, Not, And, Or, Assign]
 def emit_expr(expr: Expr):
     if isinstance(expr, Var):
         return expr.name
+    if isinstance(expr, Not):
+        return f"NOT {emit_expr(expr=expr)}"
     if isinstance(expr, And):
         return f"{emit_expr(expr.left)} AND {emit_expr(expr.right)}"
     if isinstance(expr, Or):
         return f"{emit_expr(expr.left)} OR {emit_expr(expr.right)}"
     if isinstance(expr, Assign):
-        return f"{emit_expr(expr.target)} := {emit_expr(expr.expr)}"
+        return f"{emit_expr(expr.target)} := {emit_expr(expr.expr)}\n"
--- parser.py
+++ parser.py
@@ -1,13 +1,13 @@
-from typing import List
-from classes.classes import emit_expr, Var, And, Or, Assign
+from typing import List, Optional
+from classes.classes import Expr, emit_expr, Var, And, Or, Assign
 
 class STLConverter:
     def __init__(self):
-        self.expr = None
-        self.output = []
+        self.expr: Optional[Expr] = None
+        self.output: List[str] = []
 
         # Stack to push instructions if not all conditions are made
-        self.stack = []
+        self.stack: List[Expr] = []
     
     def close_region(self):
         self.output.append(f'END_REGION')
@@ -44,13 +44,13 @@ class STLConverter:
     def store_assignment(self, operand: str):
         target = Var(operand)
         condition = emit_expr(self.expr)
-        scl_code = f"IF {condition} THEN\n{target.name} := true\nELSE\n {target.name} := false\nEND_IF;"
+        scl_code = f"IF {condition} THEN\n{target.name} := true;\nELSE\n {target.name} := false;\nEND_IF;\n"
         self.output.append(scl_code)
         self.expr = None
         self.stack.clear()
         return
     
-    def convert_bools(self, opcode, operand):
+    def convert_bools(self, opcode: str, operand: str):
         if opcode == "A":
             self.convert_and(operand)
         if opcode == "O":
@@ -58,10 +58,26 @@ class STLConverter:
         if opcode == "=":
             self.store_assignment(operand=operand)
         return
+    
+    def store_transfers(self, dest: str):
+        if self.stack:
+            var = self.stack[-1]
+            
+            self.expr = emit_expr(Assign(target=dest, expr=var))
+            self.output.append(self.expr)
+
+    def convert_transfers(self, opcode: str, operand: str):
+        if opcode == "L":
+            self.stack.append(Var(operand))
+        if opcode == "T":
+            self.store_transfers(Var(operand))
+        return self.expr
 
     def convert(self, opcode: str, operand: str):
         if opcode in ("A", "AN", "O", "ON", "SET", "CLR", "="):
             self.convert_bools(opcode=opcode, operand=operand)
+        if opcode in ("L", "T"):
+            self.convert_transfers(opcode=opcode, operand=operand)
             
     def ret_output(self):
         # print(self.output)
